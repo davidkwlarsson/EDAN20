25f0b9ac1dcd65577ebc7641236836a475556663
davidkwlarsson <dkw.larsson@gmail.com>
2019-09-04 18:50:30 +0200

    add to indexer

--- Lab1/indexer.py
+++ Lab1/indexer.py
@@ -2,6 +2,8 @@ import pickle
 import re
 import sys
 import os
+import numpy as np
+import collections as Counter
 
 def get_files(dir,suffix):
     files = []
@@ -18,21 +20,59 @@ def text_to_idx(wordIter):
             word_idx[word.group()].append(word.start())
         except:
             word_idx[word.group()] = [word.start()]
-
-    print(word_idx)
     return word_idx
 
-#files = get_files(sys.argv[1], 'txt')
-#text = open(sys.argv[1]).read()
+files = get_files(sys.argv[1], 'txt')
 
-text = open('Selma/nils.txt').read()
-WORDS = re.findall(r'\w+', text.lower())
-wordIter = re.finditer(r'\w+', text.lower())
-indexes = text_to_idx(wordIter)
+# for f in files:
+#     text = open(sys.argv[1] + '/' + f).read()
+#     #WORDS = re.findall(r'\wåäö+', text.lower())
+#     wordIter = re.finditer('\p{L}+', text.lower())
+#     indexes = text_to_idx(wordIter)
 
-
+master_indexer = {}
+#WORDS = Counter(re.findall(r'\w+', open('textfile.txt').read().lower()))
+word_counter = {}
+tot_words = {}
+for f in files:
+    text = open(sys.argv[1] + '/' + f).read()
+    count = 0
+    wordIter = re.finditer(r'\w+', text.lower())
+    indexes = text_to_idx(wordIter)
+    for word in indexes:
+        if word in master_indexer:
+            master_indexer[word][f] = indexes[word]
+            word_counter[word] += len(indexes[word])
+            count += len(indexes[word])
+        else:
+            master_indexer[word] = {}
+            word_counter[word] = 0
+            master_indexer[word][f] = indexes[word]
+            word_counter[word] += len(indexes[word])
+            count += len(indexes[word])
+    tot_words[f] = count
     
     
-#pickle.dump(WORDS, open('save.p', "wb"))
-print(WORDS)
+# pickle.dump(WORDS, open('save.p', "wb"))
+# for keys,values in word_counter.items():
+#     print(keys)
+#     print(values)
 
+ #
+ # for keys,values in master_indexer.items():
+ #     print(keys)
+
+tf_idf = {}
+for f in files:
+    text = open(sys.argv[1] + '/' + f).read()
+    wordIter = re.finditer(r'\w+', text.lower())
+    indexes = text_to_idx(wordIter)
+    for word in indexes:
+        tf_idf[word] = (len(indexes[word])/tot_words[f])*np.log(len(files)/len(master_indexer[word]))
+#
+# for keys,items in tf_idf.items():
+#     print(keys)
+#     print(items)
+
+with open("save.p", "wb") as handle:
+    pickle.dump(tf_idf, handle, protocol=pickle.HIGHEST_PROTOCOL)
