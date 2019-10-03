4239d9924b3b4dcd1cb8680d529232db7fc52387
davidkwlarsson <dkw.larsson@gmail.com>
2019-10-03 13:22:27 +0200

    Add function to train the model and save it with pickle

--- Lab5/dparser.py
+++ Lab5/dparser.py
@@ -8,7 +8,9 @@ import conll
 import features
 from sklearn.feature_extraction import DictVectorizer
 from sklearn import linear_model
+from sklearn import metrics
 import time
+import pickle
 
 
 def reference(stack, queue, graph):
@@ -57,17 +59,27 @@ if __name__ == '__main__':
     sentences = conll.read_sentences(train_file)
     formatted_corpus = conll.split_rows(sentences, column_names_2006)
 
-    # feature_names = ['stack0_POS' , 'stack0_WORD', 'queue0_POS', 'queue0_WORD', 'can-re' , 'can-la']
-    feature_names = ['stack0_POS', 'stack1_POS', 'stack0_WORD', 'stack1_WORD',
-                     'queue0_POS', 'queue1_POS', 'queue0_WORD', 'queue1_WORD', 'can-re', 'can-la']
-    # feature_names = ['stack0_POS' , 'stack1_POS', 'stack0_WORD', 'stack1_WORD',
-    #                          'queue0_POS', 'queue1_POS', 'queue0_WORD', 'queue1_WORD', 'can-re' , 'can-la']
+    # feature_names = ['stack0_POS', 'stack0_WORD', 'queue0_POS', 'queue0_WORD', 'can-re' , 'can-la']
+    # feature_names = ['stack0_POS', 'stack1_POS', 'stack0_WORD', 'stack1_WORD',
+    #                  'queue0_POS', 'queue1_POS', 'queue0_WORD', 'queue1_WORD', 'can-re', 'can-la']
+    feature_names = ['stack0_POS' , 'stack1_POS', 'stack0_WORD', 'stack1_WORD',
+                     'queue0_POS', 'queue1_POS', 'queue0_WORD', 'queue1_WORD',
+                     '1_POS', '1_WORD', '2_POS', '2_WORD', 'can-re', 'can-la']
+
+    if len(feature_names) == 6:
+        classifier_filename = "classifier1.p"
+    elif len(feature_names) == 10:
+        classifier_filename = "classifier2.p"
+    else:
+        classifier_filename = "classifier3.p"
 
     X_dict = []
     Y = []
+    DEPRELS = []
     sent_cnt = 0
     for sentence in formatted_corpus:
         sent_cnt += 1
+
         if sent_cnt % 1000 == 0:
             print(sent_cnt, 'sentences of', len(formatted_corpus), flush=True)
         stack = []
@@ -79,18 +91,25 @@ if __name__ == '__main__':
         graph['deprels']['0'] = 'ROOT'
         transitions = []
 
+        # print('Equal graphs:', transition.equal_graphs(sentence, graph))
+        x_dict = []
+        deprels = []
         while queue:
+            x = features.extract(stack, queue, graph, feature_names, sentence)
+            # x_dict.extend(x)
+            X_dict.extend(x)
             stack, queue, graph, trans = reference(stack, queue, graph)
             transitions.append(trans)
-            x = features.extract(stack, queue, graph, feature_names, sentence)
-            X_dict.extend(x)
-            Y.extend([trans])
-            # print(x, "       y = " , trans)
-
-        stack, graph = transition.empty_stack(stack, graph)
-
+            deprels.append(trans[2:])
         # print('Equal graphs:', transition.equal_graphs(sentence, graph))
 
+        # Y.extend(transitions)
+        stack, graph = transition.empty_stack(stack, graph)
+
+        # if transition.equal_graphs(sentence, graph):
+        #     X_dict.extend(x_dict)
+        DEPRELS.extend(deprels)
+        Y.extend(transitions)
         # if not transition.equal_graphs(sentence, graph):
         #     for word in sentence:
         #         print(word['form'])
@@ -104,14 +123,26 @@ if __name__ == '__main__':
         # print(transitions)
         # print(graph)
 
+    for k in range(4):
+        print(X_dict[k].values(), "  , y = ", Y[k])
+
+
     print("Encoding the features...")
     # Vectorize the feature matrix and carry out a one-hot encoding
     vec = DictVectorizer(sparse=True)
     X = vec.fit_transform(X_dict)
-
+    # print(vec.get_feature_names())
+    print(X.shape)
+    print(len(Y))
     training_start_time = time.clock()
     print("Training the model...")
-    classifier = linear_model.LogisticRegression(penalty='l2', dual=True, solver='liblinear')
+    classifier = linear_model.LogisticRegression(solver='lbfgs', multi_class='auto', n_jobs=-1)
     model = classifier.fit(X, Y)
-    print(model)
+    y_train_predict = classifier.predict(X)
+    print("Classification report for classifier %s:\n%s\n"
+          % (classifier, metrics.classification_report(Y, y_train_predict)))
+    # print(model)
+    print("writing to pickle file: --> ", classifier_filename)
+    with open(classifier_filename, "wb") as handle:
+        pickle.dump(classifier, handle)
 
