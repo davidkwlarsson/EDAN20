59b47caaced3665ac9ce7bd64e55e011d5c947f1
Samuel Lundberg <samuel@Samuels-MacBook-Air.local>
2019-10-12 18:02:01 +0200

    code had gone missing, could I use TPOT here?

---
+++ Lab3/baseline_chunker.py
@@ -0,0 +1,142 @@
+"""
+Baseline chunker for CoNLL 2000
+"""
+__author__ = "Pierre Nugues"
+import conll_reader
+
+
+def count_pos(corpus):
+    """
+    Computes the part-of-speech distribution
+    in a CoNLL 2000 file
+    :param corpus:
+    :return:
+    """
+    pos_cnt = {}
+    for sentence in corpus:
+        for row in sentence:
+            if row['pos'] in pos_cnt:
+                pos_cnt[row['pos']] += 1
+            else:
+                pos_cnt[row['pos']] = 1
+    return pos_cnt
+
+
+def train(corpus):
+    """
+    Computes the chunk distribution by pos
+    The result is stored in a dictionary
+    :param corpus:
+    :return:
+    """
+    pos_cnt = count_pos(corpus)
+#    print('corpus: ', len(corpus), '\n')
+
+    #for d in corpus[0]:
+     #   print(d)
+
+    #print('pos_cnt', pos_cnt)
+    # We compute the chunk distribution by POS
+    chunk_dist = {key: {} for key in pos_cnt.keys()}
+    #print('chunk_dist', chunk_dist)
+    for sentence in corpus:
+        for word_dict in sentence:
+            pos = word_dict['pos']
+            chunk = word_dict['chunk']
+            if chunk in chunk_dist[pos]:
+                chunk_dist[pos][chunk] += 1
+            else:
+                chunk_dist[pos][chunk] = 1
+
+
+
+    # JJR, CC, NN
+   # print('JJR: ', chunk_dist['JJR'])
+    #print('CC: ', chunk_dist['CC'])
+    #print('NN: ', chunk_dist['NN'])
+
+    """
+    Fill in code to compute the chunk distribution for each part of speech
+    """
+    # FOR EACH POS, HOW FREQUENT ARE THE CHUNKS? DICT OF DICTS
+
+    # We determine the best association
+    pos_chunk = {}
+    """
+    Fill in code so that for each part of speech, you select the most frequent chunk.
+    You will build a dictionary with key values:
+    pos_chunk[pos] = most frequent chunk for pos
+    """
+    chunk_guess = {key: '' for key in pos_cnt.keys()}
+    # Adds the most probable chunk to each pos
+    for pos in chunk_dist:
+        most_freq = 0
+        most_common = ''
+        for chunk in chunk_dist[pos]:
+            freq = chunk_dist[pos][chunk]
+            if freq > most_freq:
+                most_common = chunk
+                most_freq = freq
+        chunk_guess[pos] = most_common
+
+    return chunk_guess
+
+
+def predict(model, corpus):
+    """
+    Predicts the chunk from the part of speech
+    Adds a pchunk column
+    :param model:
+    :param corpus:
+    :return:
+    """
+    """
+    We add a predicted chunk column: pchunk
+    """
+    for sentence in corpus:
+        for row in sentence:
+            #print(model) # is empty
+            row['pchunk'] = model[row['pos']]
+    return corpus
+
+
+def eval(predicted):
+    """
+    Evaluates the predicted chunk accuracy
+    :param predicted:
+    :return:
+    """
+    word_cnt = 0
+    correct = 0
+    for sentence in predicted:
+        for row in sentence:
+            word_cnt += 1
+            if row['chunk'] == row['pchunk']:
+                correct += 1
+    return correct / word_cnt
+
+
+if __name__ == '__main__':
+    column_names = ['form', 'pos', 'chunk']
+    train_file = 'train.txt'
+    test_file = 'test.txt'
+
+    train_corpus = conll_reader.read_sentences(train_file)
+    train_corpus = conll_reader.split_rows(train_corpus, column_names)
+    test_corpus = conll_reader.read_sentences(test_file)
+    test_corpus = conll_reader.split_rows(test_corpus, column_names)
+
+    model = train(train_corpus)
+
+    predicted = predict(model, test_corpus)
+    accuracy = eval(predicted)
+    print("Accuracy", accuracy)
+    f_out = open('out', 'w')
+    # We write the word (form), part of speech (pos),
+    # gold-standard chunk (chunk), and predicted chunk (pchunk)
+    for sentence in predicted:
+        for row in sentence:
+            f_out.write(row['form'] + ' ' + row['pos'] + ' ' +
+                        row['chunk'] + ' ' + row['pchunk'] + '\n')
+        f_out.write('\n')
+    f_out.close()
