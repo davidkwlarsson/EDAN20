ede9ba993999009571301751f0c9b364946a5045
Samuel Lundberg <samuel@Samuels-MacBook-Air.local>
2019-10-12 11:51:29 +0200

    predict parsing, scores not good enough

--- Lab5/dparser2.py
+++ Lab5/dparser2.py
@@ -6,6 +6,7 @@ __author__ = "Pierre Nugues"
 import transition
 import conll
 import features
+import lab6
 
 import numpy as np
 from sklearn.feature_extraction import DictVectorizer
@@ -13,6 +14,7 @@ from sklearn import linear_model
 from sklearn import metrics
 from sklearn.naive_bayes import GaussianNB
 from sklearn.model_selection import GridSearchCV
+import pickle
 
 
 def reference(stack, queue, graph):
@@ -42,7 +44,7 @@ def reference(stack, queue, graph):
     if stack and transition.can_reduce(stack, graph):
         for word in stack:
             if (word['id'] == queue[0]['head'] or
-                        word['head'] == queue[0]['id']):
+                    word['head'] == queue[0]['id']):
                 # print('re', stack[0]['cpostag'], queue[0]['cpostag'])
                 stack, queue, graph = transition.reduce(stack, queue, graph)
                 return stack, queue, graph, 're'
@@ -52,15 +54,11 @@ def reference(stack, queue, graph):
     return stack, queue, graph, 'sh'
 
 
+def get_Xy(file, vec, classifier, column_names, feature_names, idx):
 
-def get_Xy(test_file, column_names_2006, feature_names):
+    sentences = conll.read_sentences(file)
+    formatted_corpus = conll.split_rows(sentences, column_names)
 
-    sentences = conll.read_sentences(test_file)
-    formatted_corpus = conll.split_rows(sentences, column_names_2006)
-
-    X_dict = list()
-    y = list()
-    p = 0
     for sentence in formatted_corpus:
         stack = []
         queue = list(sentence)
@@ -69,61 +67,107 @@ def get_Xy(test_file, column_names_2006, feature_names):
         graph['heads']['0'] = '0'
         graph['deprels'] = {}
         graph['deprels']['0'] = 'ROOT'
-        transitions = []
 
         while queue:
             X = features.extract(stack, queue, graph, feature_names, sentence)
-            stack, queue, graph, trans = reference(stack, queue, graph)
-            transitions.append(trans)
-            X_dict.extend(X)
-            y.extend([trans])
+            X_trans = vec.transform(X)
+            trans = classifier.predict(X_trans)
+            stack, queue, graph, trans = parse_ml(stack, queue, graph, trans[0])
 
         stack, graph = transition.empty_stack(stack, graph)
 
         for word in sentence:
+            if not 'head' in word:
+                word['head'] = '0'
+            if not 'deprel' in word:
+                word['deprel'] = 'ROOT'
+            if not 'phead' in word:
+                word['phead'] = '_'
+            if not 'pdeprel' in word:
+                word['pdeprel'] = '_'
+
+        # Does this anything
+        for word in sentence:
             word['head'] = graph['heads'][word['id']]
 
-    # print(type(X_dict[0]), X_dict[0])
-    vec = DictVectorizer(sparse=True)
-    X_mat = vec.fit_transform(X_dict)
-    # print(np.shape(X_mat))
+    out = open('system_output' + str(idx) + '.txt', 'w')
+    skipp = True
+    for sentence in formatted_corpus:
+        for word in sentence:
+            # word['head'] = graph['heads'][word['id']]
+            # word['deprel'] = graph['deprels'][word['id']]
+            if word['id'] == '0':
+                if skipp:
+                    skipp = False
+                else:
+                    out.write('\n')
 
-    return X_mat, y
+            else:
+                out.write(word['id'] + '\t' + word['form'] + '\t' + word['lemma'] + '\t' + word['cpostag']
+                          + '\t' + word['postag'] + '\t' + word['feats'] + '\t' + word['head'] + '\t' +
+                          word['deprel'] + '\t_\t_' + '\n')
+            skipp = False
+
+    out.close()
+
+    return 0
+
+
+def parse_ml(stack, queue, graph, trans):
+    if stack and trans[:2] == 'ra':     #if stack == if can_rightarc
+        queue[0]['head'] = stack[0]['id']
+        queue[0]['deprel'] = trans[3:]
+
+        stack, queue, graph = transition.right_arc(stack, queue, graph, trans[3:])
+        return stack, queue, graph, 'ra'
+    elif transition.can_leftarc(stack, graph) and trans[:2] == 'la':
+        stack[0]['head'] = queue[0]['id']
+        stack[0]['deprel'] = trans[3:]
+        stack, queue, graph = transition.left_arc(stack, queue, graph, trans[3:])
+        return stack, queue, graph, 'la'
+    elif transition.can_reduce(stack, graph) and trans == 're':
+        stack, queue, graph = transition.reduce(stack, queue, graph)
+        return stack, queue, graph, 're'
+    else:   # shift if shift or fail
+        stack, queue, graph = transition.shift(stack, queue, graph)
+        return stack, queue, graph, 'sh'
 
 
 if __name__ == '__main__':
-    train_file = 'mini.conll.txt'
-    # test_file = 'test_blind.conll.txt'
-    test_file = 'mini_test.conll.txt'
     column_names_2006 = ['id', 'form', 'lemma', 'cpostag', 'postag', 'feats', 'head', 'deprel', 'phead', 'pdeprel']
     column_names_2006_test = ['id', 'form', 'lemma', 'cpostag', 'postag', 'feats']
+    feature_names = [[], [], []]
+    feature_names[0] = ['stack0_POS', 'stack0_word', 'queue0_POS', 'queue0_word', 'can-re', 'can-la']
+    feature_names[1] = ['stack0_POS', 'stack1_POS', 'stack0_word', 'stack1_word', 'queue0_POS', 'queue1_POS',
+                        'queue0_word', 'queue1_word', 'can-re', 'can-la']
+    feature_names[2] = ['stack0_POS', 'stack1_POS', 'stack0_word', 'stack1_word', 'queue0_POS', 'queue1_POS',
+                        'queue0_word', 'queue1_word', '1_POS', '1_word', '2_POS', '2_word', 'can-re', 'can-la']
 
-    # feature_names = ['stack0_POS', 'stack0_word', 'queue0_POS', 'queue0_word', 'can-re', 'can-la']
-    feature_names = ['stack0_POS', 'stack1_POS', 'stack0_word', 'stack1_word', 'queue0_POS', 'queue1_POS',
-                       'queue0_word', 'queue1_word', 'can-re', 'can-la']
-    # feature_names = ['stack0_POS', 'stack1_POS', 'stack0_word', 'stack1_word', 'queue0_POS', 'queue1_POS',
-    #                   'queue0_word', 'queue1_word', '1_POS', '1_word', '2_POS', '2_word', 'can-re', 'can-la']
+    # file = 'train.conll.txt'
+    file = 'test_blind.conll.txt'
+    # file = 'test.conll.txt'
+    # file = 'mini.conll.txt'
+    # file = 'miniB.conll.txt'
 
+    print('models done, lets evaluate them')
+    for i in range(3):
+        # Extracting saved models and vectorizers
+        pathM = "lbfgs_model" + str(i) + ".p"
+        with open(pathM, "rb") as handle:
+            clf2 = pickle.load(handle)
+        pathV = "vec" + str(i) + ".p"
+        with open(pathV, "rb") as handle:
+            vec = pickle.load(handle)
 
-    classifier = linear_model.LogisticRegression(penalty='l2', dual=False, solver='lbfgs', max_iter=100,
-                                                 multi_class='auto')
+        get_Xy(file, vec, clf2, column_names_2006_test, feature_names[i], i)
+        print('model ' + str(i) + ' completed')
 
-    # solver = lbfgs is good. Also sag and saga is good for large scale problems. liblinear handles only binary class.
-    # multiclass: ‘auto’ selects ‘ovr’ if the data is binary, or if solver=’liblinear’, and otherwise selects ‘multinomial’
-    X_train, y_train = get_Xy(train_file, column_names_2006, feature_names)
-    model = classifier.fit(X_train, y_train)
+        # y_pred = clf2.predict(X)
+        # y_true = np.array(y)
+        # print(len(y_pred), y_pred[:10])
+        # print(len(y_train), y_train[:10])
 
-    X_test, y_test = get_Xy(test_file, column_names_2006, feature_names)
-    print(np.shape(X_train), np.shape(X_test))
-
-    y_pred = classifier.predict(X_test)
-
-    y_train = np.array(y_train)
-    print(len(y_pred), y_pred[:10])
-    print(len(y_train), y_train[:10])
-
-    report = metrics.classification_report(y_test, y_pred, labels=None, target_names=None,
-                                            sample_weight=None, digits=2, output_dict=False)
-
-    print(report)
-
+        # report = metrics.classification_report(y_true, y_pred, labels=None, target_names=None,
+                                  #             sample_weight=None, digits=2, output_dict=False)
+        # print('f1-score in the penultimate column: \n', report[-160:])
+    print('terminated succesfully')
